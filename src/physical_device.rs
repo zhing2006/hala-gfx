050f63033b651a5d6c4b7ec9af58348edf7f6c55
zhing2006 <382601573@qq.com>
2024-09-05 11:42:28 +0800

    Remove unused code from physical device.

--- src/physical_device.rs
+++ src/physical_device.rs
@@ -15,10 +15,8 @@ pub struct HalaQueueFamily {
 pub struct HalaPhysicalDevice {
   pub raw: vk::PhysicalDevice,
   pub properties: vk::PhysicalDeviceProperties,
-  pub features: vk::PhysicalDeviceFeatures,
   pub memory_properties: vk::PhysicalDeviceMemoryProperties,
   #[allow(dead_code)]
-  pub(crate) queue_families: Vec<HalaQueueFamily>,
   pub(crate) enable_buffer_device_address: bool,
   pub device_name: String,
 }
@@ -74,9 +72,7 @@ impl HalaPhysicalDevice {
       .ok_or_else(|| HalaGfxError::new("Failed to find a suitable physical device.", None))?;
 
     let (
-      features,
       memory_properties,
-      queue_families,
       device_name
     ) = Self::get_device_info(instance, physical_device, &properties);
 
@@ -93,56 +89,11 @@ impl HalaPhysicalDevice {
       Self {
         raw: physical_device,
         properties,
-        features,
         memory_properties,
-        queue_families,
         enable_buffer_device_address: features12.buffer_device_address == vk::TRUE,
         device_name,
       }
     )
-  }
-
-  /// Get the graphics family index.
-  pub fn graphics_family(&self) -> u32 {
-    self.queue_families.iter().position(|f| f.properties.queue_flags.contains(vk::QueueFlags::GRAPHICS)).unwrap() as u32
-  }
-
-  /// Get the compute family index.
-  pub fn compute_family(&self) -> u32 {
-    self.queue_families.iter().position(|f| f.properties.queue_flags.contains(vk::QueueFlags::COMPUTE)).unwrap() as u32
-  }
-
-  /// Get the transfer family index.
-  pub fn transfer_family(&self) -> u32 {
-    self.queue_families.iter().position(|f| f.properties.queue_flags.contains(vk::QueueFlags::TRANSFER)).unwrap() as u32
-  }
-
-  /// Get the max bind descriptor count.
-  pub fn max_bind_descriptor_count(&self) -> usize {
-    RESERVED_DESCRIPTOR_COUNT / 4
-  }
-
-  /// Get the max bindless descriptor count.
-  pub fn max_bindless_descriptor_count(&self) -> usize {
-    (512 * 510).min(
-      (self.properties.limits.max_per_stage_descriptor_sampled_images as usize) - RESERVED_DESCRIPTOR_COUNT)
-  }
-
-  /// Get the dynamic constants alignment.
-  pub fn dynamic_constants_alignment(&self) -> usize {
-    (256).max(
-      self.properties.limits.min_uniform_buffer_offset_alignment as usize)
-  }
-
-  /// Get the max dynamic constants bytes per dispatch.
-  pub fn max_dynamic_constants_bytes_per_dispatch(&self) -> usize {
-    (16384).min(
-      self.properties.limits.max_uniform_buffer_range as usize)
-  }
-
-  /// Get the max dynamic constants storage buffer bytes.
-  pub fn max_dynamic_constants_storage_buffer_bytes(&self) -> usize {
-    1024 * 1024
   }
 
   pub(crate) fn find_memory_type_index(
@@ -166,22 +117,12 @@ impl HalaPhysicalDevice {
     physical_device: vk::PhysicalDevice,
     properties: &vk::PhysicalDeviceProperties
   ) -> (
-    vk::PhysicalDeviceFeatures,
     vk::PhysicalDeviceMemoryProperties,
-    Vec<HalaQueueFamily>,
     String
   ) {
-    let features = unsafe { instance.raw.get_physical_device_features(physical_device) };
     let memory_properties = unsafe { instance.raw.get_physical_device_memory_properties(physical_device) };
-    let queue_families = unsafe {
-      instance.raw.get_physical_device_queue_family_properties(physical_device)
-        .into_iter()
-        .enumerate()
-        .map(|(index, properties)| HalaQueueFamily { index: index as u32, properties })
-        .collect::<Vec<_>>()
-    };
     let device_name = unsafe { std::ffi::CStr::from_ptr(properties.device_name.as_ptr()).to_str().unwrap().to_owned() };
-    (features, memory_properties, queue_families, device_name)
+    (memory_properties, device_name)
   }
 
   /// Get the device informaton2.
